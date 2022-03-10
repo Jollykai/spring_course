ac166aee25713f431e6999a3ff85c2d23c3c89c7
Losthero <ist.magna750@gmail.com>
2022-03-10 22:10:24 +0300

    [#005] Dependency Injection with setter

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -3,8 +3,17 @@ package spring_introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        System.out.println("Person bean in created");
+//        this.pet = pet;
+//    }
+
+    public Person() {
         System.out.println("Person bean in created");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
         this.pet = pet;
     }
 
