bf76d7ad3ffb627f91eec7985d02a698c93e20b8
Losthero <ist.magna750@gmail.com>
2022-03-10 22:22:48 +0300

    [#006] Injection of values

--- src/spring_introduction/Test3.java
+++ src/spring_introduction/Test3.java
@@ -13,6 +13,8 @@ public class Test3 {
 //        person.callYourPet();
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
 
         context.close();
     }
