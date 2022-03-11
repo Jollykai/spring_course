9260d112e3672bb8d81dd3211faf5053282ae3a1
Losthero <ist.magna750@gmail.com>
2022-03-11 20:06:09 +0300

    [#009] Init and destroy

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,15 +1,15 @@
 package spring_introduction;
 
 public class Dog implements Pet {
-    private String name;
+//    private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public Dog() {
         System.out.println("Dog bean is created");
@@ -18,5 +18,13 @@ public class Dog implements Pet {
     @Override
     public void say() {
         System.out.println("Bow-Wow");
+    }
+
+    public void init() {
+        System.out.println("Class:Dog Init method");
+    }
+
+    public void destroy() {
+        System.out.println("Class:Dog Destroy method");
     }
 }
--- src/spring_introduction/Test4.java
+++ src/spring_introduction/Test4.java
@@ -8,13 +8,13 @@ public class Test4 {
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
 
         Dog myDog = context.getBean("myPet", Dog.class);
-        myDog.setName("Belka");
+//        myDog.setName("Belka");
         Dog yourDog = context.getBean("myPet", Dog.class);
-        yourDog.setName("Strelka");
+//        yourDog.setName("Strelka");
         System.out.println(myDog.equals(yourDog));
-        System.out.println(myDog.getName());
+//        System.out.println(myDog.getName());
         System.out.println(myDog);
-        System.out.println(yourDog.getName());
+//        System.out.println(yourDog.getName());
         System.out.println(yourDog);
 
         context.close();
