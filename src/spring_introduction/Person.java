e68854f762ca2d3ab919f7d175fd16179fffb618
Losthero <ist.magna750@gmail.com>
2022-03-12 21:01:04 +0300

    [#012] Qualifier

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -1,25 +1,28 @@
 package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
-    @Autowired
+//    @Autowired
+//    @Qualifier("catBean")
     private Pet pet;
     private String surname;
     private int age;
 
-//    @Autowired
-//    public Person(Pet pet) {
-//        System.out.println("Person bean in created");
-//        this.pet = pet;
-//    }
+    @Autowired
+    public Person(@Qualifier("catBean") Pet pet) {
+        System.out.println("Person bean in created");
+        this.pet = pet;
+    }
 
     public Person() {
         System.out.println("Person bean in created");
     }
 //    @Autowired
+//    @Qualifier("dog")
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet");
         this.pet = pet;
