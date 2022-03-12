06f3acef83b4ad93baf146337bd754961d5b14f3
Losthero <ist.magna750@gmail.com>
2022-03-12 23:35:22 +0300

    [#014] @PostConstruct & @PreDestroy

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,6 +1,10 @@
 package spring_introduction;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class Dog implements Pet {
@@ -23,10 +27,12 @@ public class Dog implements Pet {
         System.out.println("Bow-Wow");
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("Class:Dog Init method");
     }
 
+    @PreDestroy
     public void destroy() {
         System.out.println("Class:Dog Destroy method");
     }
