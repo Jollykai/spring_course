242d2d9132e6559302ff3387e90e579003435068
Losthero <ist.magna750@gmail.com>
2022-03-13 12:56:31 +0300

    [#017] PropertySource

--- src/spring_introduction/Test6.java
+++ src/spring_introduction/Test6.java
@@ -8,7 +8,9 @@ public class Test6 {
                 new AnnotationConfigApplicationContext(MyConfig.class);
 
         Person person = context.getBean("personBean", Person.class);
-        person.callYourPet();
+        System.out.println(person.getAge());
+        System.out.println(person.getSurname());
+//        person.callYourPet();
 
 //        Pet cat = context.getBean("catBean", Pet.class);
 //        Pet cat2 = context.getBean("catBean", Pet.class);
