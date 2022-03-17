3df6bd20df428ec6987d2b3bd9ad04755cbf8990
Losthero <ist.magna750@gmail.com>
2022-03-17 11:46:34 +0300

    [#025] AfterReturning advice

---
+++ src/aop/Test2.java
@@ -0,0 +1,20 @@
+package aop;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class Test2 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        University university = context.getBean("university", University.class);
+        university.addStudents();
+        List<Student> students = university.getStudents();
+        System.out.println(students);
+//        for (Student st : students) {
+//            System.out.println(st);
+//        }
+        context.close();
+    }
+}
