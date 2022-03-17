051c67e97b7f5d76aae2d37df1247bd03663653d
Losthero <ist.magna750@gmail.com>
2022-03-17 14:33:15 +0300

    [#027] After advice

--- src/aop/aspects/UniversityLoggingAspect.java
+++ src/aop/aspects/UniversityLoggingAspect.java
@@ -1,10 +1,7 @@
 package aop.aspects;
 
 import aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -36,11 +33,17 @@ public class UniversityLoggingAspect {
 //
 //    }
 
-    @AfterThrowing(pointcut = "execution(* getStudents())",
-    throwing = "exception")
-    public void afterThrowingGetStudentLoggingAdvice(Throwable exception) {
+//    @AfterThrowing(pointcut = "execution(* getStudents())",
+//    throwing = "exception")
+//    public void afterThrowingGetStudentLoggingAdvice(Throwable exception) {
+//
+//        System.out.println("> afterThrowingGetStudentLoggingAdvice: Логируем выброс исключения - " + exception);
+//    }
 
-        System.out.println("> afterThrowingGetStudentLoggingAdvice: Логируем выброс исключения - " + exception);
+    @After("execution(* getStudents())")
+    public void afterGetStudentsLoggingAdvice() {
+        System.out.println("afterGetStudentsLoggingAdvice: " +
+                "Логируем нормальное окончание работы метода или выброс исключения");
     }
 
 }
