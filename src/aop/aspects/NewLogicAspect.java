0c36d401f759c355d8d00552bc786e25a14c2858
Losthero <ist.magna750@gmail.com>
2022-03-17 16:27:42 +0300

    [#029] Around advice. Working with exceptions

--- src/aop/aspects/NewLogicAspect.java
+++ src/aop/aspects/NewLogicAspect.java
@@ -12,13 +12,20 @@ public class NewLogicAspect {
     @Around("execution(public String returnBook())")
     public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("aroundReturnBookLoggingAdvice: В библиотеку пытаются вернуть книгу");
-        Long begin = System.currentTimeMillis();
-        Object targetMethodResult = proceedingJoinPoint.proceed();
+//        Long begin = System.currentTimeMillis();
+        Object targetMethodResult = null;
+        try {
+            targetMethodResult = proceedingJoinPoint.proceed();
+        } catch (Exception e) {
+            System.out.println("aroundReturnBookLoggingAdvice: Было поймано исключение - " + e);
+//            targetMethodResult = "Неизвестное название книги";
+            throw e;
+        }
 //        targetMethodResult = "Преступление и наказание";
-        Long end = System.currentTimeMillis();
+//        Long end = System.currentTimeMillis();
         System.out.println("aroundReturnBookLoggingAdvice: В библиотеку успещно возвращают книгу");
-        System.out.println("aroundReturnBookLoggingAdvice: Метод выполнил работу за: " +
-                (end - begin) + " миллисекунд");
+//        System.out.println("aroundReturnBookLoggingAdvice: Метод выполнил работу за: " +
+//                (end - begin) + " миллисекунд");
 
         return targetMethodResult;
 
