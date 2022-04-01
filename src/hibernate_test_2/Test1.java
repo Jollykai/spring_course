4d2da797d2c6feb39da45f85e38486d0b6d6c302
Losthero <ist.magna750@gmail.com>
2022-04-01 15:12:07 +0300

    [#036] OneToOne associations p.1

---
+++ src/hibernate_test_2/Test1.java
@@ -0,0 +1,32 @@
+package hibernate_test_2;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Aleksandr", "Ivanov", "IT", 600);
+            session.beginTransaction();
+
+            session.save(emp);
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } catch (HibernateException e) {
+            System.out.println(e);
+        } finally {
+            factory.close();
+        }
+
+    }
+}
