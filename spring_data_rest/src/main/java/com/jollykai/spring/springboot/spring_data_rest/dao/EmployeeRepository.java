6a3e4d4ff0253c35fc8f95fff8d4c4b66331aae5
Losthero <ist.magna750@gmail.com>
2022-04-13 16:26:23 +0300

    [#082] Spring Boot Actuator. End of course

--- spring_data_rest/src/main/java/com/jollykai/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
+++ spring_data_rest/src/main/java/com/jollykai/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -4,8 +4,6 @@ package com.jollykai.spring.springboot.spring_data_rest.dao;
 import com.jollykai.spring.springboot.spring_data_rest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
 
