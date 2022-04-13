00a9d938bd2a757c47d0f8a36746d589040d4e93
Losthero <ist.magna750@gmail.com>
2022-04-13 13:34:02 +0300

    [#078] Create REST API p.2

--- springboot_rest/src/main/java/com/jollykai/spring/springboot/springboot_rest/controller/MyRestController.java
+++ springboot_rest/src/main/java/com/jollykai/spring/springboot/springboot_rest/controller/MyRestController.java
@@ -20,34 +20,34 @@ public class MyRestController {
         return allEmployees;
     }
 
-//    @GetMapping("/employees/{employeeId}")
-//    public Employee getEmployee(@PathVariable int employeeId) {
-//        Employee employee = employeeService.getEmployee(employeeId);
-//
-//        return employee;
-//    }
-//
-//    @PostMapping("/employees")
-//    public Employee addNewEmployee(@RequestBody Employee employee) {
-//
-//        employeeService.saveEmployee(employee);
-//        return employee;
-//
-//    }
-//
-//    @PutMapping("/employees")
-//    public Employee updateEmployee(@RequestBody Employee employee) {
-//
-//        employeeService.saveEmployee(employee);
-//        return employee;
-//    }
-//
-//    @DeleteMapping("/employees/{employeeId}")
-//    public String deleteEmployee(@PathVariable int employeeId) {//
-//
-//        employeeService.deleteEmployee(employeeId);
-//        return "Employee with ID = " + employeeId + " was deleted";
-//
-//    }
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.getEmployee(employeeId);
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+        return employee;
+
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {//
+
+        employeeService.deleteEmployee(employeeId);
+        return "Employee with ID = " + employeeId + " was deleted";
+
+    }
 
 }
