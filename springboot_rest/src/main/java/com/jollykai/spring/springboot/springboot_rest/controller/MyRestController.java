28e0d51869e427301c74819b33433db779a6c811
Losthero <ist.magna750@gmail.com>
2022-04-13 12:46:52 +0300

    [#077] Create REST API p.1

---
+++ springboot_rest/src/main/java/com/jollykai/spring/springboot/springboot_rest/controller/MyRestController.java
@@ -0,0 +1,53 @@
+package com.jollykai.spring.springboot.springboot_rest.controller;
+
+import com.jollykai.spring.springboot.springboot_rest.entity.Employee;
+import com.jollykai.spring.springboot.springboot_rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+//    @GetMapping("/employees/{employeeId}")
+//    public Employee getEmployee(@PathVariable int employeeId) {
+//        Employee employee = employeeService.getEmployee(employeeId);
+//
+//        return employee;
+//    }
+//
+//    @PostMapping("/employees")
+//    public Employee addNewEmployee(@RequestBody Employee employee) {
+//
+//        employeeService.saveEmployee(employee);
+//        return employee;
+//
+//    }
+//
+//    @PutMapping("/employees")
+//    public Employee updateEmployee(@RequestBody Employee employee) {
+//
+//        employeeService.saveEmployee(employee);
+//        return employee;
+//    }
+//
+//    @DeleteMapping("/employees/{employeeId}")
+//    public String deleteEmployee(@PathVariable int employeeId) {//
+//
+//        employeeService.deleteEmployee(employeeId);
+//        return "Employee with ID = " + employeeId + " was deleted";
+//
+//    }
+
+}
